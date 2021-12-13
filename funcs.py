209453ccac7781e1d220585c3e6b60929730646d
nan144hz <canalnanfort@gmail.com>
2021-12-12 22:07:31 -0300

    CITF Prompt Functions #1

--- funcs.py
+++ funcs.py
@@ -1,7 +1,11 @@
 from colorama import Fore
 
 def help():
-    pass
+    print("""Syntax: citf """+Fore.LIGHTGREEN_EX+"""<command> [input files (if nescessary)]\n"""+Fore.WHITE+"""
+    \t-h, --help | Shows this help message
+    \t-v, --version  | Show the current version of CITF
+    \t-c, --compile | Compiles a .citf file (it requires a filename as argument)
+    \t-r, --read | Read and print the contents of a .citf file (it requires a filename as argument)""")
 
 def version():
     return print("CITF Compile It in Files\n"+Fore.GREEN+"Version 1.0.0 Licensed under Apache 2.0 License")
