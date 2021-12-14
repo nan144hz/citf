a26ed1d058e8e9a36bba611c32ef4fdb57d0c005
nan144hz <canalnanfort@gmail.com>
2021-12-14 11:13:20 -0300

    Coding CITF Compiler #2

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # CITF: Compile It in Files
 
 **CITF** is a prototype of a "compiler" that uses only files for compiling *.citf* files. I don't recommend using CITF now, because it is on non-released versions.
+
+<br>
+
+## How the compiler will work?
+
+**CITF** has it own prompt, with basic commands. On this prompt, you can compile files with the command below
+
+```citf -c <filename>```
+
+With this command, the prompt will call the compiler, and the compiler will compile the provided file. To compile a *.citf*, CITF copy it own compiler files to paste on the directory where the provided file is. The folder containing the compiler files is called **cenv** (like a venv, but an environment for CITF), and inside there, the compiler creates a folder called build, where are the files to compile and then execute the code.
+
+<br>
+
+## What are the files inside the **build** folder and what are they for?
+
+Let me explain with a tree
+
+```
+- code.citf (where is your code)
+- cenv (citf environment folder)
+  | - build (build folder)
+    | -> .TOKENS (where are all the tokens)
+    | -> .VARIABLES (where are all the variables)
+    | -> .CONSTANTS (where are all the constants)
+    | -> compiled.py (compiled code to execute)
+```
--- compiler/funcs.py
+++ compiler/funcs.py
@@ -1,20 +1,81 @@
-from colorama import Fore, init
-import os, sys
+# CITF Compiler ---------------------------------------------------------------------------
+# Here we have the CITF Compiler. Here is some things that you can customize to make a new
+# CITF based custom compiler
 
-# SETTINGS
-# Change here the settings of citf compiler
+# SETTINGS --------------------------------------------------------------------------------------------
+# change here the keywords, special chars, and more for your new compiler (if you want, of course)
 
 SETTINGS={
     'keywords': [],
-    'slash': [],
-    'special': [],
     'types': [],
-    'reserved': {},
-    'allowUpper': False
+    'declaration': ['const', 'let'],
+    'specialChars': {
+        'doubleQuotes': '"',
+        'leftParen': '(',
+        'rightParen': ')',
+        'leftBracket': '[',
+        'rightBracket': ']',
+        'plus': '+',
+        'minus': '-',
+        'div': '/',
+        'times': '*',
+        'greater': '>',
+        'less': '<',
+        'and': '&',
+        'or': '|',
+        'not': '!',
+        'equal': '==',
+        'exactly': '===',
+        'not_equal': '!=',
+        'not_exactly': '!==',
+        'end': ';'
+    }
 }
 
+
+
+# IMPORTATIONS -----------------------------------------------------------
+# don't change anything here
+
+from colorama import Fore, init
+import sys,os
 init(autoreset=True)
 
+
+# IMPORTANT VARS ---------------------------------------------------------
+# don't change anything here
+
+__tokens__ = []
+__variables__ = []
+__constants__ = []
+__citf__ = str(os.getcwd())
+__filename__ = ""
+__destination__ = ""
+
+
+
+# CLASSES ----------------------------------------------------------------
+# don't change anything here
+
+class Token:
+    def tokenize(value, type):
+        pass
+
+class Error:
+    def __init__(self, message):
+        return print(Fore.RED+"(COMPILER ERROR) "+Fore.WHITE+"{}".format(str(message)))
+
+
+
+# FUNCTIONS --------------------------------------------------------------
+# don't change anything here, just change if you know what are you doing
+
 def compile(f):
-    return print("""Compiling {}\n
-CITF Options: \n{}""".format(f,SETTINGS))
+    __filename__ =  f
+    __destination__= os.path.dirname(os.path.realpath(__filename__))
+
+    if os.getcwd() == __citf__:
+        os.chdir(os.getcwd()+'/compiler')
+        os.mkdir('cenv')
+        os.chdir(os.getcwd()+'/cenv')
+        os.mkdir('build')
